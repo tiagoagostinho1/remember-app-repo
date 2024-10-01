11fe6e1bf81ef1d46dfd48c16864f167f82c5138
Tiago Agostinho <46489201+tiagoagostinho1@users.noreply.github.com>
2024-10-01 14:37:57 +0100

    creating the form and list

--- remember-app/src/App.jsx
+++ remember-app/src/App.jsx
@@ -1,35 +1,86 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@mantine/core/styles.css';
+import { createTheme, MantineProvider, Textarea, Button, Group, List, Container } from '@mantine/core';
+import { DatePickerInput } from '@mantine/dates';
+import { useForm } from '@mantine/form';
+
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+let nextId = 0;
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [remForm, setRemForm] = useState(
+      {
+        id: 0,
+        remember: '',
+        date: null
+      });
+  const [items, setItem] = useState([]);
+  
+  const handleRememberChange = (e) => {
+    setRemForm({ ...remForm, remember: e.target.value });
+  };
+  const handleDateChange = (e) => {
+    setRemForm({ ...remForm, date: e.target.value });
+  };
+
+  const rememberItems = items.map((item) => <List.Item key={item.id}> {item.remember} at {item.date} </List.Item>);
+
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      inputRemember: '',
+    },
+
+    validate: {
+      inputRemember: (value) => (value.length < 2 ? 'Write at leat 2 chars' : null),
+    },
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        
+    <MantineProvider theme={theme}>
+      <Container>
+        
+        <form onSubmit={form.onSubmit(function (values) { 
+            console.log(values);
+            items.push({
+              id: nextId++,
+              remember: values.inputRemember,
+            });
+          })}>
+          <Textarea
+            label="What to remember"
+            placeholder="What to remember"
+            key={form.key('inputRemember')}
+            onChange={handleRememberChange}
+            {...form.getInputProps('inputRemember')}
+          />
+
+          <DatePickerInput
+            label="Pick date"
+            placeholder="Pick date"
+            key={form.key('date')}
+            onChange={handleDateChange}
+            {...form.getInputProps('date')}
+          />
+
+          <Group justify="flex-end" mt="md">
+            <Button type="submit">Submit</Button>
+          </Group>
+        </form>
+
+        <List>
+          {rememberItems}
+        </List>
+      </Container>
+      
+    </MantineProvider>
+  );
 }
 
 export default App
---
+++ remember-app/src/components/form/RememberForm.jsx
@@ -0,0 +1,12 @@
+import { Textarea } from '@mantine/core';
+
+function RememberForm() {
+    return (
+        <Textarea
+            label="What to remember"
+            placeholder="What to remember"
+        />
+    );
+}
+
+export default RememberForm;
