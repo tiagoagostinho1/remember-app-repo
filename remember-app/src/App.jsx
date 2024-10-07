2f2c91fc49e43346c53279bbf582cf064234b404
Tiago Agostinho <46489201+tiagoagostinho1@users.noreply.github.com>
2024-10-07 18:23:32 +0100

    moved mantine provider to main and isolated the theme in a isolated file

--- remember-app/src/App.jsx
+++ remember-app/src/App.jsx
@@ -1,26 +1,20 @@
-import { useEffect, useState } from 'react'
-import '@mantine/core/styles.css';
-import { createTheme, MantineProvider, Textarea, Button, Group, List, Container, ThemeIcon } from '@mantine/core';
-import { DatePickerInput } from '@mantine/dates';
-import { useForm } from '@mantine/form';
-import { IconArrowRight } from '@tabler/icons-react';
-
-const theme = createTheme({
-  /** Put your mantine theme override here */
-});
+import { useEffect, useState } from "react";
+import "@mantine/core/styles.css";
+import { Textarea, Button, Group, List } from "@mantine/core";
+import { DatePickerInput } from "@mantine/dates";
+import { useForm } from "@mantine/form";
+import { IconArrowRight } from "@tabler/icons-react";
 
 let nextId = 0;
 
 function App() {
-
-  const [remForm, setRemForm] = useState(
-      {
-        id: 0,
-        remember: '',
-        date: null
-      });
+  const [remForm, setRemForm] = useState({
+    id: 0,
+    remember: "",
+    date: null,
+  });
   const [items, setItems] = useState([]);
-  
+
   const handleRememberChange = (e) => {
     setRemForm({ ...remForm, remember: e.target.value });
   };
@@ -28,94 +22,100 @@ function App() {
     setRemForm({ ...remForm, date: e.target.value });
   };
 
-  const rememberItems = items.map((item) => <List.Item key={item.Code}> {item.Title} at {item.Date} </List.Item>);
+  const rememberItems = items.map((item) => (
+    <List.Item key={item.Code}>
+      {" "}
+      {item.Title} at {item.Date}{" "}
+    </List.Item>
+  ));
 
   const form = useForm({
-    mode: 'controlled',
+    mode: "controlled",
     initialValues: {
-      inputRemember: '',
+      inputRemember: "",
     },
 
     validate: {
-      inputRemember: (value) => (value.length < 2 ? 'Write at leat 2 chars' : null),
+      inputRemember: (value) =>
+        value.length < 2 ? "Write at leat 2 chars" : null,
     },
   });
 
-    useEffect(() => {
-      fetch('https://paytently-dev.outsystemsenterprise.com/Tiago_Memoir_API/rest/Memoir/GetList_Memory')
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setItems(data);
-        });
-    }, []);
-  
+  useEffect(() => {
+    fetch(
+      "https://paytently-dev.outsystemsenterprise.com/Tiago_Memoir_API/rest/Memoir/GetList_Memory"
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setItems(data);
+      });
+  }, []);
+
   return (
-        
-    <MantineProvider theme={theme}>
-      <Container>
+    <>
+      <form
+        onSubmit={form.onSubmit(function (values) {
+          //e.preventDefault();
+          console.log(values);
+          items.push({
+            id: nextId++,
+            remember: values.inputRemember,
+          });
 
-        <form onSubmit={form.onSubmit(function (values) { 
-            //e.preventDefault();
-            console.log(values);
-            items.push({
-              id: nextId++,
-              remember: values.inputRemember,
-            });
+          setRemForm({ id: 0, remember: "", date: null });
 
-            setRemForm( { id:0, remember:'', date:null } );
-            
-            fetch('https://paytently-dev.outsystemsenterprise.com/Tiago_Memoir_API/rest/Memoir/Memory', {
-               method: 'POST',
-               body: JSON.stringify({
+          fetch(
+            "https://paytently-dev.outsystemsenterprise.com/Tiago_Memoir_API/rest/Memoir/Memory",
+            {
+              method: "POST",
+              body: JSON.stringify({
                 Date: "2024-12-31",
                 Title: values.inputRemember,
                 Description: "",
               }),
-               headers: {
-                  'Content-type': 'application/json; charset=UTF-8',
-               },
-            })
+              headers: {
+                "Content-type": "application/json; charset=UTF-8",
+              },
+            }
+          );
+        })}
+      >
+        <Textarea
+          label="What to remember"
+          placeholder="What to remember"
+          key={form.key("inputRemember")}
+          value={remForm.remember}
+          onChange={handleRememberChange}
+          {...form.getInputProps("inputRemember")}
+        />
 
-          })}>
+        <DatePickerInput
+          label="Pick date"
+          placeholder="Pick date"
+          key={form.key("date")}
+          value={remForm.date}
+          onChange={handleDateChange}
+          {...form.getInputProps("date")}
+        />
 
-          <Textarea
-            label="What to remember"
-            placeholder="What to remember"
-            key={form.key('inputRemember')}
-            value={remForm.remember}
-            onChange={handleRememberChange}
-            {...form.getInputProps('inputRemember')}
-          />
+        <Group justify="flex-end" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
 
-          <DatePickerInput
-            label="Pick date"
-            placeholder="Pick date"
-            key={form.key('date')}
-            value={remForm.date}
-            onChange={handleDateChange}
-            {...form.getInputProps('date')}
-          />
-
-          <Group justify="flex-end" mt="md">
-            <Button type="submit">Submit</Button>
-          </Group>
-        </form>
-
-        <List 
-          spacing="xs"
-          size="sm"
-          center
-          icon={ <IconArrowRight style={{ width: 16, height: 16 }} />
-        }>
-          {rememberItems}
-        </List>
-      </Container>
-      
-    </MantineProvider>
+      <List
+        spacing="xs"
+        size="sm"
+        center
+        icon={<IconArrowRight style={{ width: 16, height: 16 }} />}
+      >
+        {rememberItems}
+      </List>
+    </>
   );
 }
 
-export default App
+export default App;
--- remember-app/src/main.jsx
+++ remember-app/src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Container, MantineProvider } from "@mantine/core";
+import { theme } from "./theme/theme.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <MantineProvider theme={theme}>
+    <Container>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </Container>
+  </MantineProvider>
+);
