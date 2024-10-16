f8399203304cd7d9f306784ba0ec5c8500964b29
Tiago Agostinho <46489201+tiagoagostinho1@users.noreply.github.com>
2024-10-16 14:01:56 +0100

    imports

--- remember-app/src/main.tsx
+++ remember-app/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Container, MantineProvider } from "@mantine/core";
 import { theme } from "./theme/theme";
 
-import App from "./App.js";
+import App from "./App";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <MantineProvider theme={theme}>
