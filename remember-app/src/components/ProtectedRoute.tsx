61f1832145875c721a2e71dfc87bc8a08800ff65
Tiago Agostinho <46489201+tiagoagostinho1@users.noreply.github.com>
2024-10-29 18:39:12 +0000

    change not auth

--- remember-app/src/components/ProtectedRoute.tsx
+++ remember-app/src/components/ProtectedRoute.tsx
@@ -9,17 +9,7 @@ export default function ProtectedRoute() {
   }
 
   if (!isLoading && !isAuthenticated) {
-    return (
-      <div>
-        <h1 className="text-3xl font-medium">Not authenticated</h1>
-        <button
-          onClick={() => login()}
-          className="bg-zinc-900 text-white px-3 py-2 rounded"
-        >
-          Login
-        </button>
-      </div>
-    );
+    return <h1>Not authenticated</h1>;
   }
 
   if (!isLoading && isAuthenticated) {
