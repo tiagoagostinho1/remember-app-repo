54bb6eda86f603173a46a91d42158851b3695fa2
Tiago Agostinho <46489201+tiagoagostinho1@users.noreply.github.com>
2024-10-29 15:51:56 +0000

    add nre callbacks to kinde

--- remember-app/src/main.tsx
+++ remember-app/src/main.tsx
@@ -5,13 +5,15 @@ import { theme } from "./theme/theme";
 import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 import { Router } from "./Router";
 
+const { origin } = window?.location;
+
 createRoot(document.getElementById("root") as HTMLElement).render(
   <MantineProvider theme={theme}>
     <KindeProvider
       clientId="1d5ca67b5be54e758b33e9e78e327991"
       domain="https://agostinho.kinde.com"
-      redirectUri="http://localhost:5173"
-      logoutUri="http://localhost:5173"
+      redirectUri={origin}
+      logoutUri={origin}
     >
       <Container>
         <StrictMode>
